c1dda3dfa2f1df7ea960ccc4cd7a7c2fc10c8d53
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-07-03 13:48:41 +0200

    callback GPIO

--- start_wsclient.sh
+++ start_wsclient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-/home/STRAMATEL/client/venv/bin/python /home/STRAMATEL/client/websocket-client.py
+sudo /home/STRAMATEL/client/venv/bin/python /home/STRAMATEL/client/websocket-client.py
