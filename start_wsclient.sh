18cefcb780873b8c5335e21fb8a9272c11226d26
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-07-03 13:55:37 +0200

    sudo

--- start_wsclient.sh
+++ start_wsclient.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-sudo /home/STRAMATEL/client/venv/bin/python /home/STRAMATEL/client/websocket-client.py
+/home/STRAMATEL/client/venv/bin/python /home/STRAMATEL/client/websocket-client.py
