1f655823e02e6a818203b917f57efa6155209ebc
Matthieu <matthieu.deroir@gmail.com>
2024-11-06 15:43:23 +0100

    web

--- websocket-client.py
+++ websocket-client.py
@@ -138,7 +138,7 @@ class PanelController:
         print("Rebooting panel...")
         env = os.environ.copy()
         self.current_state = None  # Reset the state after reboot
-        time.sleep(5)
+        await asyncio.sleep(1)
 
         subprocess.run(["sudo", "reboot"], env=env)
 
